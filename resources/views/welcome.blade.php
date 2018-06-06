e8f4b426db338db4d0027ddd56a7980660dad804
mikako.akai <mikako.akai@rakuten.com>
2018-06-06 05:47:46 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,3 +93,13 @@
         </div>
     </body>
 </html>
+@extends('layouts.app')
+
+@section('content')
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the kadai-tasklist</h1>
+            {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', 'TasksController@index');
 
 Route::resource('task', 'TasksController');
 
-
+// user registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
